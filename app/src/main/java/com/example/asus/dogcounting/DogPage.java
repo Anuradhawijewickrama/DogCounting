ed6c1afc6f38b40a778739e7d92151fdbfff1bb0
Jayamal Jayamaha <jayamaljayamaha2@gmail.com>
2017-11-29 00:18:36 +0530

    I added some styles to the button and also I created new activity called DogPage for enter new dog details

---
+++ app/src/main/java/com/example/asus/dogcounting/DogPage.java
@@ -0,0 +1,13 @@
+package com.example.asus.dogcounting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DogPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dog_page);
+    }
+}
