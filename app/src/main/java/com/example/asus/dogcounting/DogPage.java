ab0043c2e551ee653ee5870ef82e3eae7ff557f4
Jayamal Jayamaha <jayamaljayamaha2@gmail.com>
2017-11-29 22:01:06 +0530

    I modified dogpage for capture photo using camera and modified button styles in some activites

--- app/src/main/java/com/example/asus/dogcounting/DogPage.java
+++ app/src/main/java/com/example/asus/dogcounting/DogPage.java
@@ -1,13 +1,47 @@
 package com.example.asus.dogcounting;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class DogPage extends AppCompatActivity {
-
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    ImageView dogImageView;
+    Button button_capture;
+    Button button_galary;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dog_page);
+
+        button_capture = (Button) findViewById(R.id.button_capture);
+        button_galary = (Button) findViewById(R.id.button_galary);
+        dogImageView = (ImageView) findViewById(R.id.dogImageView);
+
+        if(!hasCamera()){
+            button_capture.setEnabled(false);
+        }
+    }
+    public boolean hasCamera(){
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_ANY);
+    }
+    public void launchCamera(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent,REQUEST_IMAGE_CAPTURE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap dogImage = (Bitmap) extras.get("data");
+            dogImageView.setImageBitmap(dogImage);
+        }
     }
 }
--- app/src/main/java/com/example/asus/dogcounting/userPage.java
+++ app/src/main/java/com/example/asus/dogcounting/userPage.java
@@ -1,7 +1,9 @@
 package com.example.asus.dogcounting;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class userPage extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class userPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_page);
+    }
+    public void enterNewDog(View view){
+        Intent intent = new Intent("com.example.asus.dogcounting.DogPage");
+        startActivity(intent);
     }
 }
